86fd44a269bec41b0937cc4a1b26c0874a974f7b
thesuhailcompany <42859854+thesuhailcompany@users.noreply.github.com>
2020-03-23 23:52:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -136,9 +136,25 @@ echo "0 0,12 * * * certbot renew >/dev/null 2>&1" >> renewcert
 crontab renewcert
 rm renewcert
 
+
+
 #restart nginx
 nginx -t
 service nginx restart
+
+#ADDING GZIP COMPRESSION SETTINGS
+
+tee -a /etc/nginx/nginx.conf > /dev/null <<EOT
+#GZIP SETTINGS
+gzip_disable "msie6";
+gzip_vary on;
+gzip_proxied any;
+gzip_comp_level 6;
+gzip_buffers 16 8k;
+gzip_http_version 1.1;
+gzip_types text/plain text/css application/json application/x-javascript text/xml application/xml application/xml+rss text/javascript;
+
+EOT
 
 fi
 
