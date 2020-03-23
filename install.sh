8cbcc66299b429c39b63537bcc66f963b196ddd3
thesuhailcompany <42859854+thesuhailcompany@users.noreply.github.com>
2020-03-23 21:44:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,11 @@ fi
 
 export DEBIAN_FRONTEND=noninteractive
 
+echo
 
-echo deploying nginx for $1
+read -p 'ENTER DOMAIN NAME WITHOUT WWW PREFIX (eg: mm.example.com) : ' DOMAIN
+
+echo
 
 sleep 3
 
@@ -18,25 +21,106 @@ apt upgrade -y
 
 apt install nginx -y
 
-mkdir /var/www/$1/
+mkdir /var/www/$DOMAIN/
 
-touch /var/www/$1/index.html
+touch /var/www/$DOMAIN/index.html
 
-tee /etc/nginx/sites-available/$1.conf > /dev/null <<EOF
+tee /etc/nginx/sites-available/$DOMAIN.conf > /dev/null <<EOF
 server {
   listen 80 default_server;
   listen [::]:80 default_server;
-  root /var/www/nginx-auto;
-  index index.html;
-  server_name $1 www.$1;
+
+  server_name $DOMAIN www.$DOMAIN;
   location / {
     try_files $uri $uri/ =404;
   }
 }
 EOF
 
-ln -s /etc/nginx/sites-available/$1.conf /etc/nginx/sites-enabled/$1.conf
+ln -s /etc/nginx/sites-available/$DOMAIN.conf /etc/nginx/sites-enabled/$DOMAIN.conf
 
+#restart nginx
+nginx -t
+
+service nginx restart
+
+#install certbot
+add-apt-repository ppa:certbot/certbot -y
+
+apt update -y
+
+apt upgrade -y
+
+apt install python-certbot-nginx -y
+
+
+#issue new certs
+
+certbot --register-unsafely-without-email --nginx certonly --agree-tos -d $DOMAIN
+
+tee /etc/nginx/sites-available/$DOMAIN.conf > /dev/null <<EOF
+
+# mattermost default port config
+
+server {
+     listen [::]:80;
+     listen 80;
+
+     server_name $DOMAIN www.$DOMAIN;
+
+     return 301 https://$DOMAIN\$request_uri;
+}
+
+server {
+     listen [::]:443 ssl;
+     listen 443 ssl;
+
+     server_name www.$DOMAIN;
+
+     ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem;
+     ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem;
+
+     ssl_protocols TLSv1.2 TLSv1.3;
+
+     add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" always;
+
+     return 301 https://$DOMAIN\$request_uri;
+}
+
+server {
+     listen [::]:443 ssl http2;
+     listen 443 ssl http2;
+
+     server_name $DOMAIN;
+
+     ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem;
+     ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem;
+
+     ssl_protocols TLSv1.2 TLSv1.3;
+
+     add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" always;
+
+    root /var/www/$DOMAIN;
+    
+    index index.html;
+
+    }
+
+EOF
+
+#write out current crontab
+crontab -l > renewcert
+
+#echo new cron into cron file
+echo "0 0,12 * * * certbot renew >/dev/null 2>&1" >> renewcert
+
+#install new cron file
+crontab renewcert
+rm renewcert
+
+#restart nginx
+nginx -t
+service nginx restart
 
 
 
