f9db5028695870d56bc72e9c4e3e6736e4adcae7
thesuhailcompany <42859854+thesuhailcompany@users.noreply.github.com>
2020-03-24 11:13:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -150,5 +150,20 @@ rm renewcert
 nginx -t
 service nginx restart
 
+#ADDING GZIP COMPRESSION SETTINGS
+
+tee /etc/nginx/conf.d/gzip.conf > /dev/null <<EOT
+#GZIP SETTINGS
+
+gzip_disable "msie6";
+gzip_vary on;
+gzip_proxied any;
+gzip_comp_level 6;
+gzip_buffers 16 8k;
+gzip_http_version 1.1;
+gzip_types text/plain text/css application/json application/x-javascript text/xml application/xml application/xml+rss text/javascript;
+
+EOT
+
 fi
 
