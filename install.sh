71ea1fef1655b75a34cc09a6144d322ef151393e
thesuhailcompany <42859854+thesuhailcompany@users.noreply.github.com>
2020-03-25 22:44:14 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,7 +24,23 @@ read -p "DOES $DOMAIN HAVE AN A RECORD POINTING TO $(curl --silent http://checki
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  # GOOD TO GO!
+  #!/bin/bash
+DOMAIN=$(dig +short $1 A | sort -n )
+
+AWS_SERVICE=$(curl -s https://checkip.amazonaws.com)
+
+if [ "$DOMAIN" == "$AWS_SERVICE" ]
+then
+echo 
+echo " ✔  A record validated for $1"
+echo
+
+DOMAIN_WWW=$(dig +short www.$1 CNAME | sort -n )
+
+if [ "$DOMAIN_WWW" == "$1." ]
+then
+echo " ✔  WWW CNAME Validated for $1" 
+echo
 
 read -p "CONTINUE INSTALLING NGINX ON $DOMAIN ? (TYPE 'Y' TO CONTINUE) : " -n 1 -r
 echo    # (optional) move to a new line
@@ -186,6 +202,10 @@ echo
 echo "Congratulations! Your NGINX is live :)"
 
 echo
+
+fi
+
+fi
 
 fi
 
